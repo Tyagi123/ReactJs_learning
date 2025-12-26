042c3cce364b2f6f7e979f2386bda3d06f8ce21c
Tyagi123 <gauravtyagi888@gmail.com>
2025-12-25 19:49:32 -0500

    Adding moviedux
    Adding moviedux

---
+++ moviedux/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import './styles.css'
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+   
+    <div className='container'>
+
+      <Header> </Header>
+
+    </div>
+
+      <footer className='footer'>
+<p>Footer content here</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
+
